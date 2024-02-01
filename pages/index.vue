2cf0e750c5a4ea83b1595caa82f5eba7f40a0be0
soareslss <soareslssa@fab.mil.br>
2024-02-01 11:15:17 -0300

    feat: implement event cover

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="d-flex h-50">
-    <v-parallax src="https://cdn.vuetifyjs.com/images/parallax/material.jpg">
+    <v-parallax :src="event.cover">
       <div
         class="d-flex flex-column fill-height justify-center align-center text-white"
       >
@@ -10,9 +10,7 @@
     </v-parallax>
   </div>
 
-  <div
-    class="d-flex justify-center align-center h-25"
-  >
+  <div class="d-flex justify-center align-center h-25">
     <v-card elevation="0">
       <v-card-title class="text-h4 text-xs-h6 font-weight-black">
         {{ event.title }}
@@ -23,11 +21,15 @@
           {{ event.startDate + ` > ` + event.endDate }}
         </div>
         <div class="my-2">
-          <v-icon icon="fa-solid fa-location-dot"/>
-          {{  event.location }}
+          <v-icon icon="fa-solid fa-location-dot" />
+          {{ event.location }}
         </div>
       </v-card-text>
     </v-card>
+  </div>
+  <div class="d-flex justify-center flex-column w-25">
+    <div class="text-h5 font-weight-black my-4">Descrição do evento</div>
+    <div> {{  event.description }}</div>
   </div>
 </template>
 <script>
@@ -38,9 +40,13 @@ export default defineComponent({
     return {
       event: {
         title: "WE MAKE BETTER DAYS l CARNIVAL EDITION 2024",
+        cover: "https://images.unsplash.com/photo-1604177052603-c2b4cff228db?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         startDate: "2024-02-09",
         endDate: "2024-02-12",
-        location: "Evento presencial em Igreja Unidade em Cristo, Rio de Janeiro - RJ"
+        location:
+          "Evento presencial em Igreja Unidade em Cristo, Rio de Janeiro - RJ",
+        description:
+          "Bem-vindos, homens de de Deus, ao primeiro culto especial com o tema 'Mover'. Nos reuniremos para juntos caminharmos a jornada de movimento em nossas vidas. Como homens, somos chamados a avançar, superar desafios e crescer em todas as áreas. Neste momento importantíssimo para nosas vidas, refletiremos sobre a coragem que nos impulsiona, a resiliência que nos move adiante e a fé que nos guia em cada passo. Que este culto inspire ação, desperte o propósito e fortaleça os laços  que nos unem como seguidores comprometidos da caminhada com Deus. Esperamos por você e te desafiamos a convidar outro homem para estar conosco. A entrada é franca, mas pedimos que traga 1Kg de alimento não perecível para ajudarmos famílias assitidas pela nossa Ação Social.",
       },
     };
   },
