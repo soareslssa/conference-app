e8a5ffa6322fc5003facb00f1550f62b25a61366
soareslssa <soareslssa.dev@gmail.com>
2024-01-31 21:29:25 -0300

    init nuxt and vuetify

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+<h1>VUETIFY</h1>
+</template>
+<script>
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+  setup() {
+    
+  },
+})
+</script>
