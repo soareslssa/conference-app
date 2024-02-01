ea9c542ebee991a1df8f13be560472432b2adb1d
soareslssa <soareslssa.dev@gmail.com>
2024-01-31 21:38:35 -0300

    feat: implement layout

---
+++ layouts/default.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <v-toolbar title="Conference" theme="dark"></v-toolbar>
+    <slot />
+    <AppFooter />
+  </div>
+</template>
+<script>
+export default {
+  setup() {
+    
+  },
+}
+</script>
